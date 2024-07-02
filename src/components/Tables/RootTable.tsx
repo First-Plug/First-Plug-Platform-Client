76ff744b6bc593ea350e7b351fd554764a8cc84e
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-07-02 15:41:22 -0300

    feat (tables): apply pagination

--- src/components/Tables/RootTable.tsx
+++ src/components/Tables/RootTable.tsx
@@ -8,6 +8,8 @@ import {
   getFilteredRowModel,
   useReactTable,
   RowData,
+  PaginationState,
+  getPaginationRowModel,
 } from "@tanstack/react-table";
 
 import {
@@ -29,6 +31,8 @@ import { Fragment, ReactNode, useState } from "react";
 import { TableType } from "@/types";
 import { TableActions } from "./TableActions";
 import HeaderFilter from "./Filters/HeaderFilter";
+import { Button } from "../ui/button";
+import { ArrowLeft, ArrowRight } from "@/common";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -36,6 +40,7 @@ interface DataTableProps<TData, TValue> {
   getRowCanExpand?: () => boolean;
   renderSubComponent?: (row: TData) => ReactNode;
   tableType: TableType;
+  pageSize?: number;
 }
 
 export function RootTable<TData, TValue>({
@@ -44,32 +49,41 @@ export function RootTable<TData, TValue>({
   getRowCanExpand,
   renderSubComponent,
   tableType,
+  pageSize = 5,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
-
+  const [pagination, setPagination] = useState<PaginationState>({
+    pageIndex: 0,
+    pageSize: pageSize,
+  });
   const table = useReactTable({
     data,
     columns,
-    getRowCanExpand,
-    getExpandedRowModel: getExpandedRowModel(),
-    getCoreRowModel: getCoreRowModel(),
-    onColumnFiltersChange: setColumnFilters,
-    getFilteredRowModel: getFilteredRowModel(),
-    columnResizeMode: "onChange",
     defaultColumn: {
       size: 200,
       minSize: 50,
       maxSize: 500,
     },
     state: {
+      pagination,
       sorting,
       columnFilters,
     },
+    autoResetPageIndex: true,
+    debugTable: true,
+    columnResizeMode: "onChange",
+    onPaginationChange: setPagination,
+    getRowCanExpand,
+    getExpandedRowModel: getExpandedRowModel(),
+    getCoreRowModel: getCoreRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
   });
 
   return (
-    <div className="h-full flex-grow  flex flex-col gap-2 ">
+    <div className="h-full flex-grow  flex flex-col  gap-2 relative  ">
       {tableType !== "subRow" && (
         <div className="   max-h-[50%] flex items-center   ">
           <TableActions table={table} type={tableType} />
@@ -161,6 +175,46 @@ export function RootTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
+      {parseInt(table.getPageCount().toLocaleString()) > 1 && (
+        <section className=" flex justify-center absolute w-full bottom-0">
+          <div className=" flex items-center gap-10">
+            <Button
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              <ArrowLeft className="w-5" />
+            </Button>
+            <span className="flex items-center gap-4">
+              {new Array(parseInt(table.getPageCount().toLocaleString()))
+                .fill("1")
+                .map((pos, i) => (
+                  <Button
+                    key={i}
+                    onClick={() =>
+                      table.setPagination({
+                        pageIndex: i,
+                        pageSize: 5,
+                      })
+                    }
+                    className={`border rounded-full grid place-items-center transition-all duration-300 ${
+                      table.getState().pagination.pageIndex === i
+                        ? "bg-blue/80 text-white"
+                        : ""
+                    }`}
+                  >
+                    {i + 1}
+                  </Button>
+                ))}
+            </span>
+            <Button
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              <ArrowRight className="w-5" />
+            </Button>
+          </div>
+        </section>
+      )}
     </div>
   );
 }
