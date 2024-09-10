a68a3100da99d14e59ed58bb28e1d1200e19a7ca
MercedesPoisson <mercedespoisson@yahoo.com>
2024-09-10 16:16:54 -0300

    download csv with product stock

--- src/services/product.services.ts
+++ src/services/product.services.ts
@@ -88,4 +88,21 @@ export class ProductServices {
     );
     return response.data;
   }
+
+  static async exportProductsCsv(): Promise<void> {
+    const response = await HTTPRequests.get(
+      `${BASE_URL}/api/products/export-csv`,
+      {
+        responseType: "blob",
+      }
+    );
+
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "products_report.csv");
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+  }
 }
