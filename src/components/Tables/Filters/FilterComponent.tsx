e85fa14f6fa1aaa55c6740f8fc3c52ad263405ce
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2024-09-24 22:36:04 -0300

    fix: filters

--- src/components/Tables/Filters/FilterComponent.tsx
+++ src/components/Tables/Filters/FilterComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SearchInput } from "../../../common/SearchInput";
 import { IconX } from "../../../common/Icons";
 
@@ -21,50 +21,22 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
   const [selectedOptions, setSelectedOptions] = useState<string[]>(
     initialSelectedOptions || []
   );
-  const [selectAll, setSelectAll] = useState<boolean>(false);
-  const [initialized, setInitialized] = useState<boolean>(false);
 
   useEffect(() => {
-    setFilteredOptions([...options]);
     setSelectedOptions(initialSelectedOptions || []);
-  }, [options, initialSelectedOptions]);
+  }, [initialSelectedOptions]);
 
-  //este useEffect hace que el selectAll y todas las opciones esten checked cuando abro el filtro
+ 
   useEffect(() => {
-    if (!initialized && options.length > 0) {
-      setSelectedOptions(options);
-      setInitialized(true);
-      onChange(options);
-    }
-  }, [options, initialized, onChange]);
+    setFilteredOptions([...options]); // Restablece las opciones filtradas a todas las opciones disponibles
+  }, [options, onClose]); // Incluimos `onClose` para restaurar cuando se cierra el modal
 
-  useEffect(() => {
-    if (
-      selectedOptions.length === filteredOptions.length &&
-      filteredOptions.length > 0
-    ) {
-      setSelectAll(true);
-    } else {
-      setSelectAll(false);
-    }
-  }, [selectedOptions, filteredOptions]);
 
   const handleSearch = (query: string) => {
     const filtered = options.filter((option) =>
       option.toLowerCase().includes(query.toLowerCase())
     );
     setFilteredOptions(filtered);
-  };
-
-  const handleSelectAll = () => {
-    if (selectAll) {
-      setSelectedOptions([]);
-      onChange([]);
-    } else {
-      setSelectedOptions(filteredOptions);
-      onChange(filteredOptions);
-    }
-    setSelectAll(!selectAll);
   };
 
   const handleCheckboxChange = (option: string) => {
@@ -78,8 +50,8 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
 
   const handleClearFilter = () => {
     setSelectedOptions([]);
-    setFilteredOptions([...options]);
-    setSelectAll(false);
+    setFilteredOptions([...options]); 
+
 
     onChange([]);
     onClearFilter();
@@ -91,10 +63,6 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
         <IconX onClick={onClose} className="cursor-pointer" />
       </div>
       <SearchInput placeholder="Search..." onSearch={handleSearch} />
-      <div className="space-x-2 mt-2 pb-1">
-        <input type="checkbox" checked={selectAll} onChange={handleSelectAll} />
-        <label>Select All</label>
-      </div>
       <div className="p-2 max-h-60 overflow-y-auto">
         {filteredOptions.map((option) => (
           <div key={option} className="mt-2 flex items-start">
@@ -111,7 +79,7 @@ const FilterComponent: React.FC<FilterComponentProps> = ({
         ))}
       </div>
       <button
-        className="mt-4 p-2 bg-red-200 hover:bg-hoverBlue  rounded"
+        className="mt-4 p-2 bg-red-200 hover:bg-hoverBlue rounded"
         onClick={handleClearFilter}
       >
         Clear Filter
