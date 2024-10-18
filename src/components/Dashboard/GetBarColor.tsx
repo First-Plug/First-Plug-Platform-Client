b42e9a108b91800f7d0f564adce49087752ab4c2
MercedesPoisson <mercedespoisson@yahoo.com>
2024-10-18 17:08:29 -0300

    WIP: Dashboard part3

--- src/components/Dashboard/GetBarColor.tsx
+++ src/components/Dashboard/GetBarColor.tsx
@@ -1,9 +1,9 @@
 export const getBarColor = (age: number, roundedAvgAge: number) => {
   if (age <= 2.5) {
-    return age <= roundedAvgAge ? "#4FE8B7" : "#d3d3d3"; // Verde
+    return age <= roundedAvgAge ? "#4FE8B7" : "#d3d3d3";
   } else if (age > 2.5 && age <= 3) {
-    return age <= roundedAvgAge ? "#f5efd0" : "#d3d3d3"; // Amarillo
+    return age <= roundedAvgAge ? "#f5efd0" : "#d3d3d3";
   } else {
-    return age <= roundedAvgAge ? "#FFC6D3" : "#d3d3d3"; // Rojo
+    return age <= roundedAvgAge ? "#FFC6D3" : "#d3d3d3";
   }
 };
