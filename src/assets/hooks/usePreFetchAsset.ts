ffd4e9ec87a560a4b039022423edb2e5968205f5
MercedesPoisson <mercedespoisson@yahoo.com>
2024-10-31 15:44:53 -0300

    better performance opening product to edit

--- src/assets/hooks/usePreFetchAsset.ts
+++ src/assets/hooks/usePreFetchAsset.ts
@@ -4,17 +4,24 @@ import { getAssetById } from "../actions";
 export const usePrefetchAsset = () => {
   const queryClient = useQueryClient();
 
-  const prefetchAsset = async (id: string) => {
-    try {
-      await queryClient.prefetchQuery({
-        queryKey: ["assets", id],
-        queryFn: () => getAssetById(id),
-        staleTime: 1000 * 60 * 10,
-        retry: 3,
-        retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
-      });
-    } catch (error) {
-      console.error(`Error prefetching asset ${id}:`, error);
+  const prefetchAsset = async (id: string, attempts = 3) => {
+    const cachedData = queryClient.getQueryData(["assets", id]);
+    if (cachedData) return cachedData;
+
+    for (let attempt = 0; attempt < attempts; attempt++) {
+      try {
+        await queryClient.prefetchQuery({
+          queryKey: ["assets", id],
+          queryFn: () => getAssetById(id),
+          staleTime: 1000 * 60 * 10,
+        });
+        return queryClient.getQueryData(["assets", id]);
+      } catch (error) {
+        console.error(
+          `Error en prefetch intento ${attempt + 1} para asset ${id}:`,
+          error
+        );
+      }
     }
   };
 
--- src/components/AsideContents/EditProductAside.tsx
+++ src/components/AsideContents/EditProductAside.tsx
@@ -2,11 +2,39 @@ import { useStore } from "@/models";
 import { observer } from "mobx-react-lite";
 import { Loader } from "../Loader";
 import ProductForm from "../AddProduct/ProductForm";
+import { useEffect, useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { Product } from "@/types";
 
 export var EditProductAside = observer(() => {
-  const {
-    products: { productToEdit },
-  } = useStore();
+  // const {
+  //   products: { productToEdit },
+  // } = useStore();
+
+  const queryClient = useQueryClient();
+  const [productToEdit, setProductToEdit] = useState<Product | null>(null);
+
+  useEffect(() => {
+    const cachedProduct = queryClient.getQueryData<Product>([
+      "selectedProduct",
+    ]);
+    if (cachedProduct) {
+      setProductToEdit(cachedProduct);
+    } else {
+      console.log("Producto no encontrado en caché, mostrando loader.");
+    }
+
+    const unsubscribe = queryClient.getQueryCache().subscribe((event) => {
+      if (
+        event.query.queryKey[0] === "selectedProduct" &&
+        event.query.state.data
+      ) {
+        setProductToEdit(event.query.state.data as Product);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [queryClient]);
 
   if (!productToEdit) return <Loader />;
 
