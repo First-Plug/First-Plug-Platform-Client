157ca310f4998f3eb8fb531a0a1e8c5321d747a0
MercedesPoisson <mercedespoisson@yahoo.com>
2025-01-20 16:42:53 -0300

    WIP: add Product Condition and Additional Info to  AddProduct form

---
+++ src/components/AddProduct/ProductCondition.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { observer } from "mobx-react-lite";
+import { Controller, useFormContext } from "react-hook-form";
+import { DropdownInputProductForm } from "@/components/AddProduct/DropDownProductForm";
+import { PRODUCT_CONDITIONS } from "@/types";
+
+const ProductCondition = function ({ isUpdate, isDisabled }) {
+  const { control } = useFormContext();
+  return (
+    <div className={` ${isUpdate ? "mb-6" : "mb-10"}`}>
+      <Controller
+        name="productCondition"
+        control={control}
+        render={({ field }) => (
+          <DropdownInputProductForm
+            options={PRODUCT_CONDITIONS}
+            placeholder="Select Product Condition"
+            title="Product Condition"
+            name="productCondition"
+            selectedOption={field.value || ""}
+            onChange={(value) => field.onChange(value)}
+            searchable={false}
+            className="w-full"
+            disabled={isDisabled}
+          />
+        )}
+      />
+    </div>
+  );
+};
+
+export default observer(ProductCondition);
