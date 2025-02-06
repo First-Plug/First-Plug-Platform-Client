8b9d41c68f4abd0bc744e996ca38381fdcd75258
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2025-02-06 12:11:36 -0300

    filters

--- src/action-history/components/table/HistoryTable.tsx
+++ src/action-history/components/table/HistoryTable.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { HistorialServices } from "@/action-history/services";
 import {
   useReactTable,
   getCoreRowModel,
   flexRender,
+  ColumnFiltersState,
 } from "@tanstack/react-table";
 
 import {
@@ -18,6 +19,10 @@ import {
 
 import { BarLoader } from "@/components/Loader/BarLoader";
 import { PaginationWithLinks } from "@/components/ui/pagination-with-links";
+import { Button } from "@/components/ui/button";
+import { DropDownArrow } from "@/common";
+import { flip, offset, shift, size, useFloating } from "@floating-ui/react-dom";
+import FilterComponent from "@/components/Tables/Filters/FilterComponent";
 
 const DEFAULT_PAGE_SIZE = 10;
 const VALID_PAGE_SIZES = [10, 25, 50];
@@ -31,7 +36,11 @@ const fetchData = async (pageIndex: number, pageSize: number) => {
   }
 };
 
-const HistoryTable = () => {
+interface Props {
+  onClearFilters?: () => void;
+}
+
+const HistoryTable = ({ onClearFilters }: Props) => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
@@ -138,10 +147,152 @@ const HistoryTable = () => {
     pageCount: totalCount,
   });
 
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [selectedFilterOptions, setSelectedFilterOptions] = useState<{
+    [key: string]: string[];
+  }>({});
+  const filterIconRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+  const [clearAll, setClearAll] = useState(false);
+  const [filterMenuOpen, setFilterMenuOpen] = useState<string | null>(null);
+  const filterRefs = useRef<{ [key: string]: HTMLDivElement | null }>({});
+  const [filterOptions, setFilterOptions] = useState<string[]>([]);
+  const handleFilterChange = (columnId: string, selectedOptions: string[]) => {
+    setSelectedFilterOptions((prev) => ({
+      ...prev,
+      [columnId]: selectedOptions,
+    }));
+
+    setColumnFilters((prev) => [
+      ...prev.filter((f) => f.id !== columnId),
+      {
+        id: columnId,
+        value: selectedOptions,
+      },
+    ]);
+  };
+
+  const { x, y, strategy, refs, update, middlewareData } = useFloating({
+    strategy: "fixed",
+    middleware: [
+      offset({
+        mainAxis: 8,
+        crossAxis: -60,
+      }),
+      flip(),
+      shift(),
+      size({
+        apply({ availableWidth, availableHeight, elements }) {
+          Object.assign(elements.floating.style, {
+            maxWidth: `${availableWidth}px`,
+            maxHeight: `${availableHeight}px`,
+          });
+        },
+      }),
+    ],
+  });
+
+  const handleFilterIconClick = (headerId: string) => {
+    if (filterMenuOpen === headerId) {
+      setFilterMenuOpen(null);
+      return;
+    }
+
+    const originalData = table.getCoreRowModel().rows.map((row) => {
+      const value = row.getValue(headerId);
+      const member = row.original;
+
+      // if (headerId === "birthDate" || headerId === "startDate") {
+      //   if (typeof value === "string" || typeof value === "number") {
+      //     const dateValue = new Date(value);
+      //     if (!isNaN(dateValue.getTime())) {
+      //       return dateValue.toLocaleString("en-US", { month: "long" });
+      //     }
+      //   }
+      //   return "No Data";
+      // }
+
+      // if (headerId === "teamId") {
+      //   if (
+      //     typeof member.team === "object" &&
+      //     member.team !== null &&
+      //     "name" in member.team
+      //   ) {
+      //     return member.team.name;
+      //   }
+      //   return "Not Assigned";
+      // }
+
+      // if (headerId === "position") {
+      //   return value ? String(value) : "No Data";
+      // }
+
+      // if (headerId === "products") {
+      //   const productCount = (member.products || []).length;
+      //   return productCount.toString();
+      // }
+
+      return value ? String(value) : "No Data";
+    });
+
+    const filteredOptions = originalData.filter(
+      (value, index, self) => self.indexOf(value) === index
+    );
+
+    const sortedOptions = filteredOptions.sort((a, b) => {
+      // if (headerId === "birthDate" || headerId === "startDate") {
+      //   const monthIndexA = MONTHS.indexOf(a);
+      //   const monthIndexB = MONTHS.indexOf(b);
+      //   if (a === "No Data") return 1;
+      //   if (b === "No Data") return -1;
+      //   return monthIndexA - monthIndexB;
+      // }
+
+      // if (headerId === "position" || headerId === "teamId") {
+      //   if (a === "No Data" || a === "Not Assigned") return 1;
+      //   if (b === "No Data" || b === "Not Assigned") return -1;
+      //   return a.localeCompare(b, undefined, { sensitivity: "base" });
+      // }
+
+      // if (headerId === "products") {
+      //   const numA = parseInt(a, 10);
+      //   const numB = parseInt(b, 10);
+      //   return numA - numB;
+      // }
+
+      return a.localeCompare(b);
+    });
+
+    if (sortedOptions.length === 0) {
+      sortedOptions.push("No Data");
+    }
+
+    setFilterOptions(sortedOptions);
+    setFilterMenuOpen(headerId);
+  };
+
+  const handleClearAllFilters = () => {
+    setColumnFilters([]);
+    setSelectedFilterOptions({});
+    setClearAll((prev) => !prev);
+    if (onClearFilters) {
+      onClearFilters();
+    }
+  };
+
   return (
     <>
       {isLoading && <BarLoader />}
       <div className="relative h-full flex-grow flex flex-col gap-1">
+        <div className="max-h-[50%] flex items-center">
+          <Button
+            onClick={handleClearAllFilters}
+            variant="outline"
+            size="sm"
+            className="mr-2"
+          >
+            Clear All Filters
+          </Button>
+        </div>
         <div className="max-h-[85%] overflow-y-auto scrollbar-custom rounded-md border w-full mx-auto">
           <Table className="w-full">
             <TableHeader>
@@ -153,12 +304,81 @@ const HistoryTable = () => {
                   {headerGroup.headers.map((header) => (
                     <TableHead
                       key={header.id}
-                      className="py-3 px-4 border-r text-start text-black font-semibold"
+                      style={{ width: `${header.getSize()}px` }}
+                      className="py-3 px-4 border-r text-start text-black font-semibold "
                     >
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext()
-                      )}
+                      <div className="flex w-full justify-between items-center">
+                        <div>
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(
+                                header.column.columnDef.header,
+                                header.getContext()
+                              )}
+                        </div>
+                        <div
+                          className={`${
+                            header.column.getCanFilter() ? "" : "hidden"
+                          }`}
+                        >
+                          {header.column.getCanFilter() && (
+                            <div
+                              className="relative"
+                              ref={(el) =>
+                                (filterIconRefs.current[header.id] = el)
+                              }
+                            >
+                              <DropDownArrow
+                                className="cursor-pointer"
+                                onClick={() => handleFilterIconClick(header.id)}
+                              />
+
+                              {filterMenuOpen === header.id && (
+                                <div
+                                  className="fixed z-40"
+                                  ref={(el) =>
+                                    (filterRefs.current[header.id] = el)
+                                  }
+                                  style={{
+                                    position: strategy,
+                                    top: y ?? 0,
+                                    left: x ?? 0,
+                                  }}
+                                >
+                                  <FilterComponent
+                                    options={
+                                      filterOptions.length > 0
+                                        ? filterOptions
+                                        : ["No Data"]
+                                    }
+                                    initialSelectedOptions={
+                                      selectedFilterOptions[header.id] || []
+                                    }
+                                    onChange={(newSelectedOptions) =>
+                                      handleFilterChange(
+                                        header.id,
+                                        newSelectedOptions
+                                      )
+                                    }
+                                    onClearFilter={() => {
+                                      setColumnFilters((prev) =>
+                                        prev.filter((f) => f.id !== header.id)
+                                      );
+
+                                      // tableType === "members"
+                                      //   ? handleFilterIconClickMembers(
+                                      //       header.id
+                                      //     )
+                                      //   : handleFilterIconClickStock(header.id);
+                                    }}
+                                    onClose={() => setFilterMenuOpen(null)}
+                                  />
+                                </div>
+                              )}
+                            </div>
+                          )}
+                        </div>
+                      </div>
                     </TableHead>
                   ))}
                 </TableRow>
--- src/app/home/activity/page.tsx
+++ src/app/home/activity/page.tsx
@@ -5,6 +5,7 @@ import { PageLayout } from "@/common";
 import { setAuthInterceptor } from "@/config/axios.config";
 
 import HistoryTable from "@/action-history/components/table/HistoryTable";
+import { FilterResetProvider } from "@/components/Tables/Filters/FilterResetContext";
 
 export default function ActionHistoryPage() {
   useEffect(() => {
@@ -16,7 +17,9 @@ export default function ActionHistoryPage() {
 
   return (
     <PageLayout>
-      <HistoryTable />
+      <FilterResetProvider>
+        <HistoryTable />
+      </FilterResetProvider>
     </PageLayout>
   );
 }
