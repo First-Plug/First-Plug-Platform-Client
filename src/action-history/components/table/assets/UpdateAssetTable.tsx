83078c156958a10656ec9d8c8ac025ccc0eefbd4
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2025-03-13 15:04:27 -0300

    fix: adquisition date

--- src/action-history/components/table/assets/UpdateAssetTable.tsx
+++ src/action-history/components/table/assets/UpdateAssetTable.tsx
@@ -93,8 +93,6 @@ const formatValue = (value: any, field?: string) => {
           day: "2-digit",
           month: "2-digit",
           year: "numeric",
-          hour: "2-digit",
-          minute: "2-digit",
         })
       : "-";
   }
