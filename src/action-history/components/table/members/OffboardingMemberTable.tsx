1495fb04793a189bda70ade5d10a9c5971dda516
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2025-03-14 14:44:09 -0300

    feat: offboarding new data in history table

--- src/action-history/components/table/members/OffboardingMemberTable.tsx
+++ src/action-history/components/table/members/OffboardingMemberTable.tsx
@@ -7,6 +7,12 @@ import {
   TableCell,
 } from "@/components/ui/table";
 import { TeamMember } from "@/types";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface MembersTableProps {
   data: {
@@ -50,43 +56,74 @@ const OffboardingMembersTable: React.FC<MembersTableProps> = ({ data }) => {
       <TableBody>
         {oldData.length === 0 ? (
           <TableRow>
-            <TableCell colSpan={4} className="h-24 text-center">
+            <TableCell colSpan={7} className="h-24 text-center">
               No members found.
             </TableCell>
           </TableRow>
         ) : (
           oldData.map((member, memberIndex) =>
-            member.products.map((product, productIndex) => (
-              <TableRow key={`${memberIndex}-${productIndex}`}>
-                <TableCell className="text-xs py-2 px-4 border-r">
-                  {member.firstName + " " + member.lastName}
-                </TableCell>
-                <TableCell className="text-xs py-2 px-4 border-r">
-                  {member.email}
-                </TableCell>
-                <TableCell className="text-xs py-2 px-4 border-r">
-                  {[
-                    product.attributes?.find((attr) => attr.key === "brand")
-                      ?.value,
-                    product.attributes?.find((attr) => attr.key === "model")
-                      ?.value,
-                    product.name,
-                  ]
-                    .filter(Boolean)
-                    .join(" ")}
-                </TableCell>
-                <TableCell className="text-xs py-2 px-4 border-r">
-                  {product.recoverable ? "Yes" : "No"}
-                </TableCell>
-                <TableCell className="text-xs py-2 px-4 border-r">
-                  {product.serialNumber || "-"}
-                </TableCell>
-                <TableCell className="text-xs py-2 px-4 border-r">
-                  {newData[memberIndex]?.products[productIndex]?.location ||
-                    "-"}
-                </TableCell>
-              </TableRow>
-            ))
+            member.products.map((product, productIndex) => {
+              const newProduct = newData[memberIndex]?.products.find(
+                (p) => (p as any).productId === product._id
+              );
+
+              const newLocation = (newProduct as any)?.newLocation || "-";
+
+              const locationToShow =
+                newLocation === "New employee" ? (
+                  newProduct?.assignedMember ? (
+                    newProduct?.assignedMember
+                  ) : (
+                    <TooltipProvider>
+                      <Tooltip delayDuration={350}>
+                        <TooltipTrigger>
+                          <span className="cursor-pointer text-sm bg-hoverRed p-1 px-3 rounded-md text-black">
+                            {newProduct?.assignedEmail} ⚠️
+                          </span>
+                        </TooltipTrigger>
+                        <TooltipContent className="bg-white">
+                          <p className="font-semibold">
+                            ❌ This email is not registered as part of your team
+                          </p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  )
+                ) : (
+                  newLocation
+                );
+
+              return (
+                <TableRow key={`${memberIndex}-${productIndex}`}>
+                  <TableCell className="text-xs py-2 px-4 border-r">
+                    {member.firstName + " " + member.lastName}
+                  </TableCell>
+                  <TableCell className="text-xs py-2 px-4 border-r">
+                    {member.email}
+                  </TableCell>
+                  <TableCell className="text-xs py-2 px-4 border-r">
+                    {[
+                      product.attributes?.find((attr) => attr.key === "brand")
+                        ?.value,
+                      product.attributes?.find((attr) => attr.key === "model")
+                        ?.value,
+                      product.name,
+                    ]
+                      .filter(Boolean)
+                      .join(" ")}
+                  </TableCell>
+                  <TableCell className="text-xs py-2 px-4 border-r">
+                    {product.recoverable ? "Yes" : "No"}
+                  </TableCell>
+                  <TableCell className="text-xs py-2 px-4 border-r">
+                    {product.serialNumber || "-"}
+                  </TableCell>
+                  <TableCell className="text-xs py-2 px-4 border-r">
+                    {locationToShow}
+                  </TableCell>
+                </TableRow>
+              );
+            })
           )
         )}
       </TableBody>
