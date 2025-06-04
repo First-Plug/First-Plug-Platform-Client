aebf2b8691b5cbceac66fc0bc3a92435203333c9
Esteban Nahuel Rodriguez <nahuelr.developer@gmail.com>
2025-06-04 17:17:03 -0300

    fix(persist-login): now persist login when the redirect url

--- src/providers/tenant.provider.tsx
+++ src/providers/tenant.provider.tsx
@@ -5,6 +5,9 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 
 import { useTenantWebSocket } from "@/hooks/useTenantWebhook";
+import { setAuthInterceptor } from "@/config/axios.config";
+import { AuthServices } from "@/services";
+import { useStore } from "@/models";
 
 interface Props {
   children: ReactNode;
@@ -13,13 +16,23 @@ interface Props {
 export const TenantProvider = ({ children }: Props) => {
   const session = useSession();
   const router = useRouter();
-
+  const {
+    user: { setUser },
+  } = useStore();
   useTenantWebSocket(session.data?.user?.tenantName);
 
   if (!session.data) return null;
   if (!session.data.user.tenantName) {
     router.push("/waiting");
     return;
+  }
+
+  sessionStorage.setItem("accessToken", session.data.backendTokens.accessToken);
+  if (sessionStorage.getItem("accessToken")) {
+    setAuthInterceptor(sessionStorage.getItem("accessToken"));
+    AuthServices.getUserInfro(session.data.user._id).then((res) => {
+      setUser(res);
+    });
   }
 
   return <>{children}</>;
